707e8704d653d48838a5b855057f87016d339391
Daniil Anfimov <anfimovdan@gmail.com>
2025-03-05 15:26:00 +0200

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='immudb_wrapper',
-    version='0.1.5',
+    version='0.1.6',
     author='Daniil Anfimov',
     author_email='anfimovdan@gmail.com',
     description=(
